d2972bb70a8079eeec3ea5e6c68a24e05a969513
David Soff <david@soff.nl>
2019-01-26 01:32:41 +0100

    Finish processbook

--- js/main.js
+++
@@ -1,6 +0,0 @@
-$(".nav-link").click(function(){
-    var target = $(this).attr("to");
-    var offset = $("#" + target).offset();
-    console.log(offset);
-   $('html, body').animate({scrollTop: offset.top - 70});
-});
